fbec446f775760aa70311abc927ee1c897c264d9
Yubo Wang <yubowang@usc.edu>
2018-04-04 22:35:58 -0700

    int to string

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ app.post('/forgot', function(req, res, next) {
         subject: 'Team6 Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + 13.57.208.67 + '/reset/' + token + '\n\n' +
+          'http://' + '13.57.208.67' + '/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
       transporter.sendMail(mailOptions, function(err) {
