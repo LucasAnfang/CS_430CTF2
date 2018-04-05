466bb8dbda6fac2c3dd90bde5494c4cad369d9cf
Luke Anfang <lanfang@usc.edu>
2018-04-04 20:40:22 -0700

    added stricter password requirements

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Webserver for secure user auth with STRONG passwords
 
 Following Guide from: http://sahatyalkabov.com/how-to-implement-password-reset-in-nodejs/
+
+To connect to mongodb compass use the following connection string:
+mongodb+srv://ctf2_t6_dev:QeBP7mFLvqL407tE@cluster0-gptnr.mongodb.net/admin
--- password-validator.js
+++ password-validator.js
@@ -1,3 +1,12 @@
+/*
+TODO add these:
+    (?=.*[A-Z].*[A-Z])        Ensure string has two uppercase letters.
+    (?=.*[!@#$&*])            Ensure string has one special case letter.
+    (?=.*[0-9].*[0-9])        Ensure string has two digits.
+    (?=.*[a-z].*[a-z].*[a-z]) Ensure string has three lowercase letters.
+    .{8}                      Ensure string is of length 8.
+*/
+
 module.exports = (password) => {
     var result = { 
         message: null,
@@ -8,30 +17,58 @@ module.exports = (password) => {
         result.valid = false;
     } 
     else {
-        re = /^\w+$/;
+        re = /.*[A-Z].*[A-Z]/;
         if(!re.test(password)) {
-            result.message = ("Error: Password must contain only letters, numbers and underscores!");
+            result.message = ("Ensure password has two uppercase letters.");
             result.valid = false;
         }
-        if(password.length < 6) {
-            result.message = ("Error: Password must contain at least six characters!");
+        if(password.length < 12) {
+            result.message = ("Ensure password contains at least twelve characters!");
             result.valid = false;
         }
-        re = /[0-9]/;
+        re = /.*[!@#$&*^-_+=:;`~,<>?].*[!@#$&*^-_+=:;`~,<>?]/;
         if(!re.test(password)) {
-            result.message = ("Error: password must contain at least one number (0-9)!");
+            result.message = ("Ensure password has two special case letter.");
             result.valid = false;
         }
-        re = /[a-z]/;
+        re = /.*[0-9].*[0-9]/;
         if(!re.test(password)) {
-            result.message = ("Error: password must contain at least one lowercase letter (a-z)!");
+            result.message = ("Ensure password has two digits!");
             result.valid = false;
         }
-        re = /[A-Z]/;
+        re = /.*[a-z].*[a-z].*[a-z]/;
         if(!re.test(password)) {
-            result.message = ("Error: password must contain at least one uppercase letter (A-Z)!");
+            result.message = ("Ensure password has three lowercase letters!");
             result.valid = false;
         }
     } 
     return result;
 }
+
+/*
+Original:
+    re = /^\w+$/;
+    if(!re.test(password)) {
+        result.message = ("Error: Password must contain only letters, numbers and underscores!");
+        result.valid = false;
+    }
+    if(password.length < 12) {
+        result.message = ("Error: Password must contain at least twelve characters!");
+        result.valid = false;
+    }
+    re = /[0-9]/;
+    if(!re.test(password)) {
+        result.message = ("Error: password must contain at least one number (0-9)!");
+        result.valid = false;
+    }
+    re = /[a-z]/;
+    if(!re.test(password)) {
+        result.message = ("Error: password must contain at least one lowercase letter (a-z)!");
+        result.valid = false;
+    }
+    re = /[A-Z]/;
+    if(!re.test(password)) {
+        result.message = ("Error: password must contain at least one uppercase letter (A-Z)!");
+        result.valid = false;
+    }
+*/
