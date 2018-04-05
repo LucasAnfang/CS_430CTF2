0f1185b52b56d96d60364edb5289b12607633d35
Yueh-Hsun Lin <yuehhsul@usc.edu>
2018-04-04 21:16:24 -0700

    app.js fix

--- app.js
+++ app.js
@@ -138,11 +138,11 @@ app.post('/login', function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
     if (err) return next(err)
     if (!user) {
-      return res.redirect('/login')
+      return res.redirect('/failure')
     }
     req.logIn(user, function(err) {
       if (err) return next(err);
-      return res.redirect('/failure');
+      return res.redirect('/');
     });
   })(req, res, next);
 });
