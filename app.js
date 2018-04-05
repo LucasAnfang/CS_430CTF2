79256626c672c2c8311e656bfaeb4b77e70fa4cc
Yueh-Hsun Lin <yuehhsul@usc.edu>
2018-04-04 20:39:46 -0700

    reverted change

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ app.post('/signup', function(req, res) {
   else {
     user.save(function(err) {
       req.logIn(user, function(err) {
-        res.redirect('/success');
+        res.redirect('/');
       });
     });
   }
