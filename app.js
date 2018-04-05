263e263d1106b7b6569f59fdb7b5a005d0115fc5
Yubo Wang <yubowang@usc.edu>
2018-04-04 22:34:28 -0700

    reset link

--- app.js
+++ app.js
@@ -221,11 +221,11 @@ app.post('/forgot', function(req, res, next) {
       });
       var mailOptions = {
         to: user.email,
-        from: 'passwordreset@demo.com',
-        subject: 'Node.js Password Reset',
+        from: 'passwordreset@csci430Team6.com',
+        subject: 'Team6 Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'http://' + 13.57.208.67 + '/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
       transporter.sendMail(mailOptions, function(err) {
